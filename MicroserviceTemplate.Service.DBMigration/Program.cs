3da26eb7cc6f85e67427aa524274fabdf171641d
Luke Cusolito <Luke.Cusolito@outlook.com>
2018-02-23 06:44:38 +1100

    Add DbUp project

---
+++ MicroserviceTemplate.Service.DBMigration/Program.cs
@@ -0,0 +1,50 @@
+﻿using DbUp;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+
+namespace MicroserviceTemplate.Service.DBMigration
+{
+    class Program
+    {
+        const string DBConnectionStringName = "MicroserviceTemplateDbContext";
+        static int Main(string[] args)
+        {
+            var connectionString =
+                args.FirstOrDefault()
+                ?? ConfigurationManager.ConnectionStrings[DBConnectionStringName].ConnectionString;
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+
+                #if DEBUG
+                Console.ReadLine();
+                #endif
+
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
